0eb37a6fafc9efc3ad3ccfac6fdb66065c78a731
Andrés Patiño <andres@frontporchsolutions.com>
2023-11-20 16:06:49 -0500

    feat: :sparkles: Render the site logo within Astro using the Astro Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+
+const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: [IMAGE_DOMAIN],
+  },
   integrations: [tailwind({
     applyBaseStyles: false,
   })]
