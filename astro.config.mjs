6f0af357fed5ee9b07727d388e4f52eedb5d2580
Andrés Patiño <andres@frontporchsolutions.com>
2023-11-28 16:59:40 -0500

    feat: :sparkles: Map Wordpress page routes to use Astro site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { loadEnv } from "vite";
 import react from "@astrojs/react";
-const {
-  IMAGE_DOMAIN
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { PUBLIC_WP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: [IMAGE_DOMAIN]
+    domains: [PUBLIC_WP_URL]
   },
   integrations: [tailwind({
     applyBaseStyles: false
